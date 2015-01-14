88e24665d5bde2a72764e886a8eadfa859f3cd42
Eddy Essien <eessien@coverity.com>
2015-01-14 13:03:09 -0700

    Virtualenv support added via virtualfish

--- config.fish
+++ config.fish
@@ -3,3 +3,8 @@
 if test -e ~/.profile.fish
 	source ~/.profile.fish
 end
+
+# Virtualfish
+source virtualfish/global_requirements.fish
+source virtualfish/projects.fish
+source virtualfish/virtual.fish
